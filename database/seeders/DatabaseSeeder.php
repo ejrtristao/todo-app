03539ec26c79fdfa0bcc71ff13c75c5fbf20809f
ejrtristao <ejrtristao@gmail.com>
2022-06-16 13:59:29 -0300

    feat: include seeds and fakers to todo and users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,11 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->count(5)->create();
+        \App\Models\Todo::factory()->count(30)->create();
     }
 }
