ab87c41e8d5e86b3119136fce7474fee312db9ac
ejrtristao <ejrtristao@gmail.com>
2022-06-16 15:12:51 -0300

    feat: create todo controller

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTodoRequest;
+use App\Models\Todo;
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Todo::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreTodoRequest $request)
+    {
+        $todo = Todo::create($request->all());
+        return response()->json($todo, 201);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function update(StoreTodoRequest $request, Todo $todo)
+    {
+        $todo->update($request->all());
+        return response()->json($todo, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Todo $todo)
+    {
+        $todo->delete();
+        return response()->json(null, 204);
+    }
+}
