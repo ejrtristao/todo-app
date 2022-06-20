efb0565a6a13e69887915f2343d0f906379ff1b6
ejrtristao <ejrtristao@gmail.com>
2022-06-20 18:03:23 -0300

    fix: include middleware

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,10 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TodoController;
 use Illuminate\Support\Facades\Route;
 
-// Route::group(['middleware' => 'auth:api'], function () { 
+Route::group(['middleware' => 'auth:api'], function () { 
     Route::apiResource('todos', TodoController::class);
     Route::put('todos/completed/{todo}', [TodoController::class, 'completed']);
-// });
+});
 
 Route::controller(AuthController::class)->group(function () {
     Route::post('login', 'login')->name('login');
