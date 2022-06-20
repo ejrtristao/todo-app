dcb027a8e284d960ad1487e68c09b5b45d24dbc3
ejrtristao <ejrtristao@gmail.com>
2022-06-20 08:52:10 -0300

    feat: create todo index page

---
+++ resources/js/components/todos/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Tarefas Novas</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <todos></todos>
+            </div>
+        </div>
+    </div>
+</template>
